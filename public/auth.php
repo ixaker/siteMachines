ef79c544cfbf4c37d92d49eaa7515ceab0094f13
xaker <xaker.dnepr@gmail.com>
2024-12-28 16:58:32 +0200

    fix

--- public/auth.php
+++ public/auth.php
@@ -1,29 +1,29 @@
-
 <?php
-// Хэш заранее известного пароля
+
 $storedHash = '$2y$10$vCddfHT9K5KtkXh7OQpMaeKdhAsxyDQ6r0WNa9zG07J3uWcIwDn3y'; // test
 
-// Стартуем сессию для защиты и удобного хранения данных
-session_start();
+header('Content-Type: application/json');
 
-// Проверяем, был ли отправлен POST-запрос
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $inputPassword = $_GET['password'] ?? '';
 
-    // Сравниваем введённый пароль с сохранённым хэшем
     if (password_verify($inputPassword, $storedHash)) {
-        header('Content-Type: application/json');
-        echo json_encode(['status' => 'success']);
-        
+        echo json_encode([
+            'status' => 'success',
+            'message' => 'Пароль верный'
+        ]);
     } else {
-        // Неверный пароль
-        header('Content-Type: application/json');
-        http_response_code(401); // Код ответа "Unauthorized"
-        echo json_encode(['status' => 'error', 'message' => 'Неверный пароль!']);
+        http_response_code(401);
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Неверный пароль'
+        ]);
     }
 } else {
-    // Если не GET-запрос, возвращаем ошибку
-    header('Content-Type: application/json');
-    http_response_code(405); // Код ответа "Method Not Allowed"
-    echo json_encode(['status' => 'error', 'message' => 'Используйте только GET-запрос.']);
+    http_response_code(405);
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Используйте только GET-запрос'
+    ]);
 }
+?>
